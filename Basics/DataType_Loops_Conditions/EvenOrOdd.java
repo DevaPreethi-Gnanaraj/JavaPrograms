3244922e274f071e46e1fd1140c68778af71a7a6
Deva Preethi <g.devapreethi@gmail.com>
2025-11-16 21:30:36 +0530

    Implement Even or Odd number printer
    The program prompts the user for an end value and prints all even and odd numbers from 0 to that value.

--- Basics/DataType_Loops_Conditions/EvenOrOdd.java
+++ Basics/DataType_Loops_Conditions/EvenOrOdd.java
@@ -1,1 +1,36 @@
 //Program to find Even or Odd numbers from 1 to 100
+package JavaPractice;
+
+import java.util.Scanner;
+
+public class PrintEvenOrOdd {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the range end value:");
+		Scanner input=new Scanner(System.in);
+		int n=input.nextInt();
+		System.out.println("Even Numbers");
+		for(int i=0;i<=n;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.print(i+" ");
+			}
+			
+		}
+		System.out.println();
+		System.out.println("Odd Numbers");
+		for(int i=0;i<=n;i++)
+		{
+			if(i%2!=0)
+			{
+				System.out.print(i+" ");
+			}
+			
+		}
+		input.close();
+
+	}
+
+}
+
